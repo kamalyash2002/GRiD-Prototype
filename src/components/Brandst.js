9fa03899c366aff1be3ef39a2fcfa82e3d823bc8
CJC0DeS <jauharivatika@protonmail.com>
2023-08-20 04:06:25 +0530

    jhasirkijai

--- src/components/Brandst.js
+++ src/components/Brandst.js
@@ -1,15 +1,55 @@
-import React from "react";
+import React from 'react'
 
 export default function Brandst() {
   return (
-    <div>
-      <div class="reward-container mb-3">
-        Exciting Rewards for you
-        <br />
-        <button type="button" class="btn btn-primary">
-          View Claimed Rewards
-        </button>
-      </div>
+     //root div
+     <div>
+
+
+
+<div class="container text-center">
+  <div class="row">
+    <div class="col">
+    <div class="card h-100" >
+     <img src="https://upload.wikimedia.org/wikipedia/commons/1/13/Swiggy_logo.png" class="card-img-top"  />
+    <div class="card-body">
+    <h5 class="card-title">Get 10% off on Swiggy</h5>
+    <p class="card-text">Place order of Rs. 200/- or more and get 10% off .</p>
+    <a href="#" class="btn btn-primary">Redeem Now</a>
+    </div>
+    </div>
+    </div>
+    <div class="col">
+    <div class="card h-100" >
+     <img src="https://upload.wikimedia.org/wikipedia/commons/2/24/Adidas_logo.png" class="card-img-top"  />
+    <div class="card-body">
+    <h5 class="card-title">Get 15% off on Adidas</h5>
+    <p class="card-text">Get 15% off upto Rs 1000/- on purchase of Sports shoes worth Rs 2000/- or above.</p>
+    <a href="#" class="btn btn-primary">Redeem Now</a>
+    </div>
+    </div>
+    </div>
+    <div class="col">
+    <div class="card h-100" >
+     <img src="https://purepng.com/public/uploads/large/purepng.com-mcdonalds-logologobrand-logoiconslogos-25151994043391qrj.png" class="card-img-top"  />
+    <div class="card-body">
+    <h5 class="card-title">Get 10% off on McDonalds</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Redeem Now</a>
+    </div>
+    </div>
+    </div>
+  </div>
+</div>
+  
+
+
+  
+  
+
+  
+    
+
     </div>
   );
 }
